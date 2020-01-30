8a810986a4beb40bd8750e2828a9e4725b0f3535
yee2542 <yee2542gtl@gmail.com>
2020-01-30 19:28:15 +0700

    init project

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+console.log('hello kaa');
+import { euei, eiei } from './interfaces/test.interface';
+
+const test = (s: eiei) => {
+  console.log(s);
+};
+
+test({ msg: 'testja' });
---
+++ src/interfaces/test.interface.ts
@@ -0,0 +1,7 @@
+export interface euei {
+  test: boolean;
+}
+
+export interface eiei {
+  msg: string;
+}
