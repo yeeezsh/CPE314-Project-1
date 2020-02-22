ed7dffd41a7f2138f26f69e837e7f33ac97c600e
topty <topty@live.com>
2020-02-22 20:13:21 +0700

    Test readline

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
-console.log('hello kaa');
+import * as readline from 'readline';
 import { euei, eiei } from './interfaces/test.interface';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('', line => {
+  console.log('input: ', line);
+  rl.close();
+});
 
 const test = (s: eiei) => {
   console.log(s);
