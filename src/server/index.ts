4b39f16ec7e79847d33c77d211429392d01dcb02
yee2542 <yee2542gtl@gmail.com>
2020-02-24 23:05:51 +0700

    set to ipv4 only

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,13 @@ console.log('server ja');
 import net = require('net');
 
 const PORT = 5000;
+const HOST = '0.0.0.0';
 
 const server = net.createServer();
 
-server.listen(PORT, () => console.log('server start at ', server.address()));
+server.listen(PORT, HOST, () =>
+  console.log('server start at ', server.address()),
+);
 
 server.on('connection', socket => {
   server.getConnections((err, n) => {
