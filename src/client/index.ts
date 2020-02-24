dae5d6d46d518a9a776dd50aa77e05d357fd07fb
yee2542 <yee2542gtl@gmail.com>
2020-02-24 15:01:30 +0700

    add simple publish data

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,29 @@
+import net = require('net');
 console.log('client ja');
+
+const END_POINT = '127.0.0.1';
+const PORT = 8080;
+
+// net.connect(END_POINT, () => console.log('connecting to ', END_POINT));
+// const connect = net.connect(PORT, END_POINT);
+const client = net.connect(PORT, END_POINT, () => {
+  console.log('create connection to', END_POINT);
+});
+
+client.once('connect', () => {
+  console.log('establish connection');
+  client.write('PUBLISH');
+  client.end(() => console.log('end write'));
+});
+
+client.on('error', err => {
+  console.log('error connection retrying');
+  setTimeout(() => client.connect(PORT, END_POINT).setTimeout(500), 1000);
+});
+
+client.on('close', hadError => {
+  if (hadError) return console.log('connection closed with error');
+  return console.log('connection closed without error');
+});
+
+client.on('end', () => console.log('end connection'));
