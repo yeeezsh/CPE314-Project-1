563cf0aac7459c89d5e24614b223da8eeaee239d
yee2542 <yee2542gtl@gmail.com>
2020-02-24 22:53:43 +0700

    add simple publish method

--- src/client/cmd.action.ts
+++ src/client/cmd.action.ts
@@ -2,23 +2,33 @@ import { Socket } from 'net';
 import { PORT } from '.';
 
 const ON_SUB = false;
-const MAX_RETRY = 2;
+const MAX_RETRY = 3;
 export type ActionType = 'publish' | 'subscribe' | string;
 export default (socket: Socket, s: ActionType, ...options: string[]) => {
   let currentRetry = 0;
   const targetIp = options[0];
 
-  socket.on('error', err => {
-    console.log('connecting...');
-    setTimeout(() => {
-      currentRetry++;
-      if (currentRetry <= MAX_RETRY) {
-        socket.connect(PORT, targetIp).setTimeout(1000);
-      } else {
-        throw `cannot connect to ${targetIp}`;
-      }
-    }, 1000);
-  });
+  // socket.setTimeout(2000);
+  // socket.on('close', () => console.log('on close'));
+
+  // socket.on('error', err => {
+  //   console.log('connecting...');
+  //   // setTimeout(() => {
+  //   //   currentRetry++;
+  //   //   socket.connect(PORT, targetIp, );
+  //   //   if (currentRetry > MAX_RETRY) {
+  //   //     socket.end();
+  //   //     throw `cannot connect to ${targetIp}`;
+  //   //   }
+  //   // }, 1000);
+  // });
+
+  // socket.on('error', e => {
+  //   socket.setTimeout(1000, () => {
+  //     socket.connect(PORT, targetIp);
+  //   });
+  //   console.log('retry');
+  // });
 
   // if (!ON_SUB) {
   //   socket.connect(PORT, targetIp, () =>
@@ -27,15 +37,17 @@ export default (socket: Socket, s: ActionType, ...options: string[]) => {
   // }
 
   console.log('resting param', options);
+
   switch (s) {
     case 'publish':
-      socket.connect(PORT, targetIp, () => {
-        console.log('connected to ', targetIp);
-        socket.write('write ja');
-        socket.end(() => console.log('closed connection'));
-      });
-      console.log('end conn');
-      console.log('publish action');
+      // socket.connect(PORT, targetIp, () => {
+      console.log('connected to ', targetIp);
+      console.log('establish connection');
+      socket.write('write ja');
+      socket.end();
+      // socket.end(() => console.log('closed connection'));
+      // });
+
       return;
     case 'subscribe':
       console.log('subscribe action');
