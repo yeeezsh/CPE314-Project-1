99ce1bbeb7079f427faf09e0919c233b2696b36f
yee2542 <yee2542gtl@gmail.com>
2020-02-24 18:19:18 +0700

    refactor

--- src/client/index.ts
+++ src/client/index.ts
@@ -11,7 +11,7 @@ console.log('Hostname :', os.hostname());
 console.log('Local IP :', IPv4, '\n');
 
 const END_POINT = '127.0.0.1';
-export const PORT = 8080;
+export const PORT = 5000;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -25,49 +25,13 @@ const mainCmd = (cb?: (s: any) => void) => {
     let client = undefined;
     const target = options[0];
     if (!client) client = net.connect(PORT, target);
-    try {
-      cmdAction(client, action, ...options);
-    } catch (err) {
-      throw err;
-    }
+    cmdAction(client, action, ...options);
+
     rl.emit('line');
     return cb && cb(line);
   });
 };
 
-// const client = net.connect(PORT, END_POINT, () =>
-//   console.log('create connection to', END_POINT),
-// );
-
 rl.on('line', () => mainCmd());
-try {
-  mainCmd();
-} catch (err) {
-  console.log('retry');
-  //   console.error(err);
-  mainCmd();
-}
 
-// client.once('connect', () => {
-//   console.log('establish connection');
-//   mainCmd();
-//   //
-//   //   client.write('PUBLISH');
-//   //   client.end(() => console.log('end write'));
-// });
-
-// client.on('close', () => console.log('on closed conn'));
-
-// client.on('error', err => {
-//   console.log('error connection retrying');
-//   setTimeout(() => client.connect(PORT, END_POINT).setTimeout(500), 1000);
-// });
-
-// client.on('close', hadError => {
-//   if (hadError) return console.log('connection closed with error');
-//   return console.log('connection closed without error');
-// });
-
-// client.on('end', () => {
-//   console.log('\n end connection');
-// });
+mainCmd();
