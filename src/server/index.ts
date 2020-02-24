2d7d434437cd81d520429b0d13173d6c11018b3e
yee2542 <yee2542gtl@gmail.com>
2020-02-24 15:01:42 +0700

    add simple server listen

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,29 @@
 console.log('server ja');
+import net = require('net');
+
+const PORT = 8080;
+
+const server = net.createServer();
+
+server.listen(PORT, () => console.log('server start at ', server.address()));
+
+server.on('connection', socket => {
+  server.getConnections((err, n) => {
+    console.log('number of connection', n);
+  });
+  socket.on('connect', () =>
+    console.log(
+      'client',
+      socket.remoteAddress,
+      socket.remoteFamily,
+      socket.remotePort,
+    ),
+  );
+
+  socket.on('data', data => {
+    //   socket
+    // console.log(socket.);
+    console.log('on data', data.toString());
+  });
+  // console.log('on connection', socket.on('connect'))
+});
