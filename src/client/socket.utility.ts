aa22a4b2c3a3d7df48bb6271ca455c1a21ca95cd
yee2542 <yee2542gtl@gmail.com>
2020-03-07 21:16:44 +0700

    add socket util

--- src/client/socket.utility.ts
+++ src/client/socket.utility.ts
@@ -9,3 +9,32 @@ export const connectToBroker = async (
     socket.on('connect', () => resolve(socket));
     socket.on('error', () => reject());
   });
+
+interface SubSocket {
+  ip: string;
+  s: net.Socket;
+}
+type SocketList = SubSocket[];
+class BrokerSubsrcibe {
+  list: SocketList;
+  constructor() {
+    this.list = [];
+  }
+
+  addSub(ip: string, s: net.Socket) {
+    this.list.push({ ip, s });
+    return;
+  }
+
+  getByIp(ip: string): SubSocket {
+    return this.list.filter(f => f.ip === ip)[0];
+  }
+
+  exist(ip: string): boolean {
+    const ips = this.list.map(e => e.ip);
+    if (ips.includes(ip)) return true;
+    return false;
+  }
+}
+
+export const brokerSubsrcibe = new BrokerSubsrcibe();
