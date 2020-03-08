43dc22c3f8d6bba8cdf640917e533ed95bc9052d
yee2542 <yee2542gtl@gmail.com>
2020-03-08 20:54:22 +0700

    refaactor jaaa

--- src/server/index.ts
+++ src/server/index.ts
@@ -35,50 +35,49 @@ server.on('connection', socket => {
   });
 
   console.log(
-    '[CONN] inncoming from ',
-    socket.remoteAddress,
-    ':',
+    `[CONN] inconmming from ${socket.remoteAddress}:`,
     socket.remotePort,
   );
 
-  // socket.on('connect', () => {
-  //   console.log('[CONN] connection establish');
-  // });
-
   socket.on('data', data => {
     console.log(
-      '[CONN] incomming data from ',
-      socket.remoteAddress,
-      ':',
+      `[CONN] incomming data from ${socket.remoteAddress}:`,
       socket.remotePort,
     );
     const parsed = new SocketData(data.toString());
 
     /* quick handle */
+    const topic = parsed.getTopic();
+    const msg = parsed.getMessage();
     switch (parsed.getAction()) {
       case 'publish':
-        Subscriber.publish(parsed.getTopic(), parsed.getMessage());
+        Subscriber.publish(topic, msg);
+        console.log(`[PUB] from ${socket.remoteAddress}:`, socket.remotePort);
+        console.log(`Topic: ${topic} | Msg: ${msg}`);
         break;
       case 'subscribe':
         new Subscriber(parsed.getTopic(), socket);
+        console.log(`[SUB] from ${socket.remoteAddress}:`, socket.remotePort);
+        console.log(`Topic: ${topic}`);
         break;
       default:
-        console.error('unrecognized action:', parsed.getAction());
+        console.error('[ERR] unrecognized action:', parsed.getAction());
     }
-
-    console.log('on data', parsed.getMessage());
-    console.log('topic:', parsed.getTopic());
   });
 
   socket.on('close', () => {
-    console.log('client on close', socket.address()),
-      server.getConnections((err, n) => {
-        if (err) {
-          console.log('[ERR] ', err);
-        }
-        console.log('[CONN] number of connection', n);
-      });
-    socket.destroy();
+    console.log(
+      `[CONN] disconnected from ${socket.remoteAddress}:`,
+      socket.remotePort,
+    );
+
+    server.getConnections((err, n) => {
+      if (err) {
+        console.log('[ERR] ', err);
+      }
+      console.log('[CONN] number of connection', n);
+    });
+    Subscriber.remove(socket);
   });
   // console.log('on connection', socket.on('connect'))
 });
