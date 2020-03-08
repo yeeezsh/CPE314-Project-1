8d6eafc3ab63b3a1f859aa230a3e7b80d7991a68
yee2542 <yee2542gtl@gmail.com>
2020-03-08 16:07:50 +0700

    remove used file

--- src/server/parse.action.ts
+++
@@ -1,9 +0,0 @@
-// SCHEMA
-// ACTION TOPIC MSG
-export default (msg: string): 'publish' | 'subscribe' => {
-  const parse = msg.split(' ');
-  const action = parse[0];
-  if (action === 'publish') return 'publish';
-  if (action === 'subscribe') return 'subscribe';
-  throw new Error('invalid msg parser');
-};
